6d1f99a355075e9c8111e02be4cde336b8198d90
djfusco <davidjfusco@gmail.com>
2023-12-15 18:37:17 -0500

    api additions

---
+++ api/chatcall.js
@@ -0,0 +1,32 @@
+export default async function handler(req, res) {
+    const search = req.query.search || '';
+    const url = 'https://api.openai.com/v1/chat/completions';
+    const data =
+    {                       
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": search}],
+        "temperature": 0.7
+    };
+    
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 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',
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(result => {
+        res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+        res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+        res.json(result.choices[0].message.content);
+        console.log(result.choices[0].message.content);
+        })
+      .catch(error => {
+        console.error('Error:', error);
+      }); 
+}
--- src/hax-gpt.js
+++ src/hax-gpt.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit';
+import "@lrnwebcomponents/multiple-choice/multiple-choice.js";
 
 export class HaxGPT extends LitElement {
   static get properties() {
@@ -20,8 +21,8 @@ export class HaxGPT extends LitElement {
     this.version = null;
   }
 
-  async _postxAPIStatement(e) {
-    console.log(e.detail);
+  async _callGPT(e) {
+    //console.log(e.detail);
     let base = ''; 
     if (
       window.location.origin.startsWith("http://127.0.0.1") ||
@@ -31,26 +32,54 @@ export class HaxGPT extends LitElement {
         .replace(/127.0.0.1:8(.*)/, "localhost:3000")
         .replace(/localhost:8(.*)/, "localhost:3000");
     }
-    return await fetch(`${base}/api/sheet?search=insert`).then((r) => r.ok ? r.json() : []).then((data) => {
+    const topic = this.shadowRoot.querySelector('#topicTextBox').value;
+    return await fetch(`${base}/api/chatcall?search=${topic}`).then((r) => r.ok ? r.json() : []).then((data) => {
       return data;
     });
   }
 
+
+  _getRandomTopic() {
+    fetch('https://api.publicapis.org/random')
+      .then(response => response.json())
+      .then(data => {
+        const randomTopic = data.entries[0].Description; // Extracting a random topic
+        this.shadowRoot.querySelector('#topicTextBox').value = randomTopic;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+
+  firstUpdated() {
+    this._getRandomTopic(); 
+    const button = this.shadowRoot.querySelector('#myButton');
+    if (button) {
+      button.addEventListener('click', async () => {
+        const results = await this._callGPT();
+        const box = this.shadowRoot.querySelector('#myTextBox');
+        if (box) {
+         box.value = results; 
+        }
+
+      });
+    }
+  }
+
   render() {
     return html`
-      <multiple-choice
-        correct-text="You got a meal deal"
-        incorrect-text="You did not get a meal deal...."
-        hide-title
-        @user-engagement="${this._postxAPIStatement}"
-        >
-        <input type="checkbox" value="Option 1 - Correct answer" correct>
-        <input type="checkbox" value="Option 2">
-        <input type="checkbox" value="Option 3">
-        <input type="checkbox" value="Option 4">
-      </multiple-choice>
+
+    <input type="text" style="width: 1300px; height: 140px;" id="topicTextBox">
+    Write a refection statement about this topic.
+    <input type="text" style="width: 1300px; height: 140px;" id="topicAnswer">
+
+
+    <button id="myButton">Get Help</button>
+    <input type="text" style="width: 1300px; height: 140px;" id="myTextBox">
     `;
   }
+
 }
 
 customElements.define('hax-gpt', HaxGPT);
